28d9ab07ec3abb0d58c9c1a8fb73d3a4dcaf5af6
Tomasz Kowal <tomaszkowal@Admins-MacBook-Pro-5.local>
2015-08-29 12:56:10 +0200

    tick the time and gather input

--- Clock.elm
+++ Clock.elm
@@ -1,9 +1,10 @@
 import Html exposing (div, button, input, text, Html)
 import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 import Time exposing (..)
     
 type Action = Tick | Increment | Input String
-type alias Model = (Int, String)
+type alias Model = { counter: Int, input: String }
 
 -- main function emits Html over time (like in react)
 main : Signal Html
@@ -26,15 +27,15 @@ modelUpdatesToView userActionsMailboxAddress modelUpdates =
 
 initialModel : Model
 initialModel =
-  (100, "We need more time")
+  { counter = 100, input = "We need more time" }
   
 -- all user inputs need to go to mailbox expecting Action
 -- view takes mailbox and model and turns into html
 view : Signal.Address Action -> Model -> Html
 view userActionsMailboxAddress model =
-  div [] [text (toString (counter model))
-         , myButton userActionsMailboxAddress
-         , myInput userActionsMailboxAddress]
+  div [] [div [] [text (toString model.counter)]
+         , div [] [myButton userActionsMailboxAddress model.input]
+         , div [] [myInput userActionsMailboxAddress]]
 
 -- address is a mailbox expecting Actions (Signal Action)
 -- currently it does nothing
@@ -42,25 +43,25 @@ myInput : Signal.Address Action -> Html
 myInput userActionsMailboxAddress =
   input [on "input"
             targetValue
-            (\input -> Signal.message userActionsMailboxAddress (Input input))] []
+            (\input -> Signal.message userActionsMailboxAddress (Input input))
+        , type' "number" ] []
 
 -- address is a mailbox expecting Actions (Signal Action)
 -- button click sends Increment action
-myButton : Signal.Address Action -> Html
-myButton userActionsMailboxAddress =
-  button [onClick userActionsMailboxAddress Increment] [text "We need more time"]
+myButton : Signal.Address Action -> String -> Html
+myButton userActionsMailboxAddress userInput =
+  button [onClick userActionsMailboxAddress Increment] [text userInput]
 
 -- update takes action and model and returns new model
 update : Action -> Model -> Model
 update action model =
-  (counter, input) = model
   case action of
     Tick ->
-      (counter - 1, input)
+      { model | counter <- model.counter - 1 } 
     Increment ->
-      (counter + 1, input)
+      { model | counter <- model.counter + 1 } 
     Input string ->
-      (counter, input)
+      { model | input <- string }
 
 -- merges user actions and ticks
 actions : Signal Action -> Signal Action
@@ -71,12 +72,3 @@ actions userActions =
 ticks : Signal Action
 ticks =
   Signal.map (\tick -> Tick) (every second)
-
-counter : Model -> Int
-counter model =
-  (counter, input) = model
-  counter
-
-input : Model -> String
-  (counter, input) = model
-  input
