933864c94ad59576b97653fa30c5ea893cdccd3a
Tomasz Kowal <tomasz.kowal@erlang-solutions.com>
2015-08-29 15:23:37 +0200

    Finished game

--- Clock.elm
+++ Clock.elm
@@ -1,10 +1,17 @@
+import String
 import Html exposing (div, button, input, text, Html)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Time exposing (..)
+import Random exposing (..)
+import Signal exposing (Signal, (<~))
     
-type Action = Tick | Increment | Input String
-type alias Model = { counter: Int, input: String }
+type Action = Tick Float | Increment | Input String
+type alias Multiplication = (Int, Int)
+type alias Model = { counter: Int
+                   , input: String
+                   , currentSeed: Random.Seed
+                   , multiplication: Multiplication }
 
 -- main function emits Html over time (like in react)
 main : Signal Html
@@ -12,7 +19,7 @@ main =
   let
     -- the only place, where I can do something with side effects
     -- creating mailbox, that will passed to view
-    userActionsMailbox = Signal.mailbox Tick
+    userActionsMailbox = Signal.mailbox (Tick 0)
     -- create signal of model updates
     modelUpdates = 
       Signal.foldp update initialModel (actions userActionsMailbox.signal)
@@ -27,13 +34,18 @@ modelUpdatesToView userActionsMailboxAddress modelUpdates =
 
 initialModel : Model
 initialModel =
-  { counter = 100, input = "We need more time" }
+  { counter = 100
+    -- fst (generate (int 0 10) (initialSeed 1))
+    , input = "We need more time"
+    , currentSeed = initialSeed 0
+    , multiplication = (10, 5) }
   
 -- all user inputs need to go to mailbox expecting Action
 -- view takes mailbox and model and turns into html
 view : Signal.Address Action -> Model -> Html
 view userActionsMailboxAddress model =
   div [] [div [] [text (toString model.counter)]
+         , div [] [text (stringFromMultiplication model.multiplication)]
          , div [] [myButton userActionsMailboxAddress model.input]
          , div [] [myInput userActionsMailboxAddress]]
 
@@ -56,12 +68,14 @@ myButton userActionsMailboxAddress userInput =
 update : Action -> Model -> Model
 update action model =
   case action of
-    Tick ->
-      { model | counter <- model.counter - 1 } 
+    Tick timeStamp ->
+      { model |
+                counter <- model.counter - 1
+              , currentSeed <- initialSeed (round timeStamp) } 
     Increment ->
       { model | counter <- model.counter + 1 } 
     Input string ->
-      { model | input <- string }
+      handleInput string model
 
 -- merges user actions and ticks
 actions : Signal Action -> Signal Action
@@ -71,4 +85,40 @@ actions userActions =
 -- creates signal of ticks
 ticks : Signal Action
 ticks =
-  Signal.map (\tick -> Tick) (every second)
+  Signal.map (\(timeStamp, tick) -> Tick timeStamp) (timestamp (every second))
+
+-- seed : Signal Random.Seed
+-- seed = (\ (t, _) -> Random.initialSeed <| round t) <~ Time.timestamp (Signal.constant ())
+
+stringFromMultiplication : Multiplication -> String
+stringFromMultiplication multiplication =
+  toString (fst multiplication) ++ "x" ++ toString (snd multiplication)
+
+resultOfMultiplication : Multiplication -> Int
+resultOfMultiplication multiplication =                      
+  (fst multiplication) * (snd multiplication)
+
+compareInputWithMultiplication : Multiplication -> String -> Bool
+compareInputWithMultiplication multipliction userInput = 
+  case String.toInt userInput of
+    Ok integer ->
+      integer == resultOfMultiplication multipliction
+    Err reason ->
+      False
+
+handleInput : String -> Model -> Model
+handleInput userInput model =
+  case compareInputWithMultiplication model.multiplication userInput of
+    True ->
+      { model | counter <- model.counter + 1
+        , multiplication <- generateMultiplication model.currentSeed }
+    False ->
+      model
+
+generateMultiplication : Random.Seed -> Multiplication
+generateMultiplication seed0 =                        
+  let
+    (first, seed1) = generate (int 0 10) seed0
+    (second, seed2) = generate (int 0 10) seed1 
+  in
+    (first, second)
